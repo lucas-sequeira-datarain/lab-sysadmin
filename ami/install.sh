e1ae2dfa2727b6e719073a2c3f9c7a4fa5d33f5a
lucasnseq <43300925+lucasns97@users.noreply.github.com>
2022-04-12 14:28:15 -0300

    Update

--- ami/install.sh
+++ ami/install.sh
@@ -3,4 +3,5 @@ sudo apt update
 sudo apt install python3-pip -y
 sudo apt install nginx -y
 pip3 install virtualenv
-virtualenv app-env
+virtualenv lab-sysadmin-env
+source lab-sysadmin-env/bin/activate
--- ami/user-data.sh
+++ ami/user-data.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-source appserver/bin/activate
+source lab-sysadmin-env/bin/activate
+git clone https://github.com/lucas-sequeira-datarain/lab-sysadmin.git
+cd lab-sysadmin
+pip3 install -r application/requirements.txt
