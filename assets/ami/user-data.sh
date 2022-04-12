15307ad8304ffe49fdebb53e5c76808bc547ce87
lucasnseq <43300925+lucasns97@users.noreply.github.com>
2022-04-12 15:50:29 -0300

    Update

--- assets/ami/user-data.sh
+++ assets/ami/user-data.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
-source lab-sysadmin-env/bin/activate
+
+# Clone repo
 git clone https://github.com/lucas-sequeira-datarain/lab-sysadmin.git
 cd lab-sysadmin
+
+# Install dependencies
+source lab-sysadmin-env/bin/activate
 pip3 install -r application/requirements.txt
-sudo cp assets/ami/lab-sysadmin-appserver.service /etc/systemd/system/lab-sysadmin-appserver.service
-systemctl start lab-sysadmin-appserver
+
+# Create /var/www/application
+sudo cp -r application /var/www/application
+cd /var/www/application
+sudo chown -R www-data:www-data /var/www/application/
+cd ~/lab-sysadmin
+
+# Configure Nginx
+
+# .conf file
+sudo cp assets/ami/application.conf /etc/nginx/sites-available/application.conf
+sudo ln -s /etc/nginx/sites-available/application.conf /etc/nginx/sites-enabled/
+sudo systemctl restart nginx
 000000
-systemctl daemon-reload
+
+# .service file
+sudo cp assets/ami/lab-sysadmin-appserver.service /etc/systemd/system/application.service
+sudo systemctl restart application.service
 000000
-sudo cp assets/ami/lab-sysadmin-appserver /etc/nginx/sites-available/lab-sysadmin-appserver
-sudo ln -s /etc/nginx/sites-available/lab-sysadmin-appserver /etc/nginx/sites-enabled
-systemctl restart nginx
+sudo systemctl daemon-reload
 000000
