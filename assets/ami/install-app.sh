db40a4bf4ee29330b317ae6ed2099bd29bd99466
lucasnseq <43300925+lucasns97@users.noreply.github.com>
2022-04-12 16:18:48 -0300

    Update

--- assets/ami/install-app.sh
+++ assets/ami/install-app.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
-source lab-sysadmin-env/bin/activate
+echo "[ACTIVATE ENVIRONMENT]"
+source lab-sysadmin-env/bin/activate > tmp.txt
 
 # Update repo
+echo "[UPDATE REPOSITORY]"
 cd ~/lab-sysadmin
-git pull
+git pull > tmp.txt
 
 # Install dependencies
-pip3 install -r application/requirements.txt
+echo "[UPDATE DEPENDENCIES]"
+pip3 install -r application/requirements.txt > tmp.txt
 
 # Create /var/www/application
+echo "[CREATE /VAR/WWW/APPLICATION]"
 sudo cp -r application /var/www/application
 cd /var/www/application
-sudo chown -R www-data:www-data /var/www/application/
+sudo chown -R www-data:www-data /var/www/application/ > tmp.txt
 cd ~/lab-sysadmin
 
 # Configure Nginx
 
 # Configuration
-sudo systemctl start nginx
-sudo systemctl enable nginx
+echo "[INTIALIZING NGINX]"
+sudo systemctl start nginx > tmp.txt
+sudo systemctl enable nginx > tmp.txt
 
 # .conf file
+echo "[CONFIGURING NGINX .CONF]"
 sudo rm -r /etc/nginx/sites-available/application.conf
 sudo cp assets/ami/application.conf /etc/nginx/sites-available/application.conf
-sudo ln -s /etc/nginx/sites-available/application.conf /etc/nginx/sites-enabled/
-sudo systemctl restart nginx
+sudo ln -s /etc/nginx/sites-available/application.conf /etc/nginx/sites-enabled/ > tmp.txt
+sudo systemctl restart nginx > tmp.txt
 
 # .service file
+echo "[CONFIGURING NGINX .SERVICE]"
 sudo rm -r /etc/systemd/system/application.service
 sudo cp assets/ami/application.service /etc/systemd/system/application.service
-sudo systemctl restart application.service
-sudo systemctl daemon-reload
+sudo systemctl restart application.service > tmp.txt
+sudo systemctl daemon-reload > tmp.txt
--- assets/ami/user-data.sh
+++ assets/ami/user-data.sh
@@ -5,5 +5,4 @@ sudo rm -r lab-sysadmin
 git clone https://github.com/lucas-sequeira-datarain/lab-sysadmin.git
 
 # Run install-app.sh
-sudo chmod +x lab-sysadmin/assets/ami/install-app.sh
 bash lab-sysadmin/assets/ami/install-app.sh
