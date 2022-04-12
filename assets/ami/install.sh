439035e1881a906fbde86d653f9ed5be61a950cf
lucasnseq <43300925+lucasns97@users.noreply.github.com>
2022-04-12 15:12:29 -0300

    Update

--- assets/ami/install.sh
+++ assets/ami/install.sh
@@ -3,9 +3,13 @@ sudo su -
 passwd ubuntu
 000000
 000000
+exit
 sudo apt update
 sudo apt install python3-pip -y
 sudo apt install nginx -y
-pip3 install virtualenv
+sudo apt install python3-virtualenv
+pip uninstall -y virtualenv
+pip3 uninstall -y virtualenv
+pip install virtualenv
 virtualenv lab-sysadmin-env
 source lab-sysadmin-env/bin/activate
--- assets/ami/user-data.sh
+++ assets/ami/user-data.sh
@@ -6,3 +6,7 @@ pip3 install -r application/requirements.txt
 sudo cp assets/ami/lab-sysadmin-appserver.service /etc/systemd/system/lab-sysadmin-appserver.service
 systemctl start lab-sysadmin-appserver
 000000
+sudo cp assets/ami/lab-sysadmin-appserver /etc/nginx/sites-available/lab-sysadmin-appserver
+sudo ln -s /etc/nginx/sites-available/lab-sysadmin-appserver /etc/nginx/sites-enabled
+systemctl restart nginx
+000000
