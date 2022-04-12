06139e835c5b577dee5ebc02fde2e0e780d7e8d5
lucasnseq <43300925+lucasns97@users.noreply.github.com>
2022-04-12 14:39:18 -0300

    Update

--- assets/ami/user-data.sh
+++ assets/ami/user-data.sh
@@ -3,3 +3,4 @@ source lab-sysadmin-env/bin/activate
 git clone https://github.com/lucas-sequeira-datarain/lab-sysadmin.git
 cd lab-sysadmin
 pip3 install -r application/requirements.txt
+sudo cp assets/ami/lab-sysadmin-appserver.service /etc/systemd/system/lab-sysadmin-appserver.service
