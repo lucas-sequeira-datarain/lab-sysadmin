721df88552035ae038f0a912bd6b372ec82d53ea
lucasnseq <43300925+lucasns97@users.noreply.github.com>
2022-04-12 15:37:30 -0300

    update

--- assets/ami/user-data.sh
+++ assets/ami/user-data.sh
@@ -6,6 +6,8 @@ pip3 install -r application/requirements.txt
 sudo cp assets/ami/lab-sysadmin-appserver.service /etc/systemd/system/lab-sysadmin-appserver.service
 systemctl start lab-sysadmin-appserver
 000000
+systemctl daemon-reload
+000000
 sudo cp assets/ami/lab-sysadmin-appserver /etc/nginx/sites-available/lab-sysadmin-appserver
 sudo ln -s /etc/nginx/sites-available/lab-sysadmin-appserver /etc/nginx/sites-enabled
 systemctl restart nginx
