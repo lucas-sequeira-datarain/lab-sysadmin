13d5ff93ca3a8edf694e952ea2931c41141d03d4
lucasnseq <43300925+lucasns97@users.noreply.github.com>
2022-04-12 16:19:54 -0300

    Update install-app.sh

--- assets/ami/install-app.sh
+++ assets/ami/install-app.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 echo "[ACTIVATE ENVIRONMENT]"
-source lab-sysadmin-env/bin/activate > tmp.txt
+source lab-sysadmin-env/bin/activate
 
 # Update repo
 echo "[UPDATE REPOSITORY]"
 cd ~/lab-sysadmin
-git pull > tmp.txt
+git pull
 
 # Install dependencies
 echo "[UPDATE DEPENDENCIES]"
-pip3 install -r application/requirements.txt > tmp.txt
+pip3 install -r application/requirements.txt
 
 # Create /var/www/application
 echo "[CREATE /VAR/WWW/APPLICATION]"
 sudo cp -r application /var/www/application
 cd /var/www/application
-sudo chown -R www-data:www-data /var/www/application/ > tmp.txt
+sudo chown -R www-data:www-data /var/www/application/
 cd ~/lab-sysadmin
 
 # Configure Nginx
 
 # Configuration
 echo "[INTIALIZING NGINX]"
-sudo systemctl start nginx > tmp.txt
-sudo systemctl enable nginx > tmp.txt
+sudo systemctl start nginx
+sudo systemctl enable nginx
 
 # .conf file
 echo "[CONFIGURING NGINX .CONF]"
 sudo rm -r /etc/nginx/sites-available/application.conf
 sudo cp assets/ami/application.conf /etc/nginx/sites-available/application.conf
-sudo ln -s /etc/nginx/sites-available/application.conf /etc/nginx/sites-enabled/ > tmp.txt
-sudo systemctl restart nginx > tmp.txt
+sudo ln -s /etc/nginx/sites-available/application.conf /etc/nginx/sites-enabled/
+sudo systemctl restart nginx
 
 # .service file
 echo "[CONFIGURING NGINX .SERVICE]"
 sudo rm -r /etc/systemd/system/application.service
 sudo cp assets/ami/application.service /etc/systemd/system/application.service
-sudo systemctl restart application.service > tmp.txt
-sudo systemctl daemon-reload > tmp.txt
+sudo systemctl restart application.service
+sudo systemctl daemon-reload
