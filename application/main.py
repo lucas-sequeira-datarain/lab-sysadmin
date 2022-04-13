9b4bdb161e6f935356e253d4c91721383f6b5fb5
lucasnseq <43300925+lucasns97@users.noreply.github.com>
2022-04-13 08:56:41 -0300

    update

--- application/main.py
+++ application/main.py
@@ -3,6 +3,7 @@ from src.metrics import get_ec2_metrics
 import pyexcel as pe
 from io import StringIO
 from flask_cors import CORS
+import os
 
 # EB looks for an 'app' callable by default.
 app = Flask(__name__)
@@ -12,7 +13,13 @@ CORS(app)
 @app.route('/')
 def home():
     resp = Response("Home")
-    resp.headers['X-Private-IP'] = 'MY.EC2.PRIVATE.IP'
+
+    # Get the private IP (from hostname)
+    hostname = os.system("hostname") # hostname: ip-10-0-0-0
+    ec2_private_ip = '.'.join(hostname.split('-')[1:]) # ip: 10.0.0.0
+
+    # Add to header
+    resp.headers['X-Private-IP'] = ec2_private_ip
     return resp
 
 # Health Check
